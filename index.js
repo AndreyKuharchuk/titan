58c5e7d008cd36dbafab26927ce72a3c211b9e95
Andrey Kuharchuk <a_kuharchuk@twistellar.com>
2021-05-26 21:51:30 +0300

    2nd commit

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
+const { Client } = require('pg');
+
+const client = new Client({
+    user: 'postgres',
+    host: 'database-2.c2kjmrriyglx.us-east-2.rds.amazonaws.com',
+    database: 'titan',
+    password: 'zXJiF9tAPiwUEcjjXjjw',
+    port: 5432,
+});
+
+const dbres = client.connect();
+
 app.get('/', (req, res) => {
+    console.log(dbres);
     res.send('HOME PAGE');
 
 });
