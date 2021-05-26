0070d0fc7e69e17bc4ff45616a5900051e02d1d0
Andrey Kuharchuk <a_kuharchuk@twistellar.com>
2021-05-26 23:46:44 +0300

    3rd

---
+++ titan/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const { Client } = require('pg');
+
+const client = new Client({
+    user: 'postgres',
+    host: 'database-2.c2kjmrriyglx.us-east-2.rds.amazonaws.com',
+    database: 'titan',
+    password: 'zXJiF9tAPiwUEcjjXjjw',
+    port: 5432,
+});
+
+const dbres = client.connect();
+
+app.get('/  ', (req, res) => {
+    console.log(dbres);
+    res.sendFile(__dirname + "home.html");
+
+});
+
+app.listen(3000, () => {
+    console.log(`HELLO`);
+});
+
